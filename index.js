e7eb4837b28c80da9abe976b2886b66a23a218ba
KrishJoshi <tuffleo@gmail.com>
2019-04-09 09:43:15 +0100

    init

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 // DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
 
 exports.puppeteerscreenshot = require("./puppeteer/screenshot.js").handler;
+exports.puppeteerHelloWorld = require("./puppeteer/HelloWorld.js").handler;
